b199391b521a790c26631a91240e669571d28bc1
Gabriel Linder <linder.gabriel@gmail.com>
2026-05-18 12:28:23 +0200

    AtomicBool is overkill here.
    Signed-off-by: Gabriel Linder <linder.gabriel@gmail.com>

--- src/logs.rs
+++ src/logs.rs
@@ -3,12 +3,10 @@
 //! This module provides helpers for development and troubleshooting support.
 
 use std::sync::LazyLock;
-use std::sync::atomic::AtomicBool;
 
 /// A flag indicating whether debug logging is enabled.
 /// This is determined by the presence of the `DEBUG` environment variable.
-pub static DEBUG_ENABLED: LazyLock<AtomicBool> =
-    LazyLock::new(|| AtomicBool::new(std::env::var("DEBUG").is_ok()));
+pub static DEBUG_ENABLED: LazyLock<bool> = LazyLock::new(|| std::env::var("DEBUG").is_ok());
 
 /// Print debug messages to stderr if `DEBUG` environment variable is set.
 ///
@@ -25,7 +23,7 @@ pub static DEBUG_ENABLED: LazyLock<AtomicBool> =
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)*) => {
-        if $crate::logs::DEBUG_ENABLED.load(std::sync::atomic::Ordering::Relaxed) {
+        if *$crate::logs::DEBUG_ENABLED {
             eprintln!($($arg)*);
         }
     };
